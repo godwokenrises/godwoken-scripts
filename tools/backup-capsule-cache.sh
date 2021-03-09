ab81a93c1642f17d7dd8a5a554453c4ff53b2234
jjy <jjyruby@gmail.com>
2021-03-09 12:26:21 +0800

    Fix CI environments

--- tools/backup-capsule-cache.sh
+++ tools/backup-capsule-cache.sh
@@ -1,1 +1,1 @@
-docker run --rm -v /tmp:/backup -v capsule-cache:/volume-data -w /volume-data busybox sh -c "tar -cvf /backup/capsule-cache.tar *"
+docker run --rm -v `pwd`/.tmp:/backup -v capsule-cache:/volume-data -w /volume-data busybox sh -c "tar -cvf /backup/capsule-cache.tar *"
--- tools/restore-capsule-cache.sh
+++ tools/restore-capsule-cache.sh
@@ -1,1 +1,1 @@
-docker run --rm -v capsule-cache:/volume-data -w/volume-data -v /tmp/capsule-cache.tar:/backup/cache.tar busybox tar -xvf /backup/cache.tar
+docker run --rm -v capsule-cache:/volume-data -w/volume-data -v `pwd`/.tmp/capsule-cache.tar:/backup/cache.tar busybox tar -xvf /backup/cache.tar
