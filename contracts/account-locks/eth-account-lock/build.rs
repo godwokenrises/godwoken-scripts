0227c82dfe7c51696444ec1daf2085aab79fe7a3
jjy <jjyruby@gmail.com>
2021-04-28 09:37:58 +0000

    Integrate secp256k1 into eth-account-lock

---
+++ contracts/account-locks/eth-account-lock/build.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dir = Path::new(&dir)
+        .parent().unwrap()
+        .parent().unwrap()
+        .parent().unwrap()
+        .join("c/build");
+    println!("cargo:rustc-link-search=native={}", dir.display());
+    println!("cargo:rustc-link-lib=static=ckb-secp256k1");
+}
