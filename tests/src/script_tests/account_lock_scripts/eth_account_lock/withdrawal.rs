a15c2408b589dee46a1efc7de9c9d0d8ca23802a
jjy <jjyruby@gmail.com>
2021-06-15 20:46:41 +0800

    tests: Verify signature without challenge context

--- tests/src/script_tests/account_lock_scripts/eth_account_lock/withdrawal.rs
+++ tests/src/script_tests/account_lock_scripts/eth_account_lock/withdrawal.rs
@@ -46,9 +46,7 @@ fn verify_withdrawal_signature(
             .build()
     };
     // rollup lock & config
-    let challenge_lock_type = build_type_id_script(b"challenge_lock_type_id");
     let eth_lock_type = build_type_id_script(b"eth_lock_type_id");
-    let challenge_script_type_hash: [u8; 32] = challenge_lock_type.calc_script_hash().unpack();
     let eth_lock_type_hash: [u8; 32] = eth_lock_type.calc_script_hash().unpack();
     let allowed_eoa_type_hashes: Vec<Byte32> = vec![Pack::pack(&eth_lock_type_hash)];
     let rollup_config = RollupConfig::new_builder()
@@ -137,32 +135,9 @@ fn verify_withdrawal_signature(
     // deploy scripts
     let param = CellContextParam {
         eth_lock_type: eth_lock_type.clone(),
-        always_success_type: challenge_lock_type.clone(),
         ..Default::default()
     };
     let mut ctx = CellContext::new(&rollup_config, param);
-    let challenge_capacity = 10000_00000000u64;
-    let challenged_block = chain.local_state().tip().clone();
-    let challenge_target_index = 0u32;
-    let input_challenge_cell = {
-        let lock_args = ChallengeLockArgs::new_builder()
-            .target(
-                ChallengeTarget::new_builder()
-                    .target_index(Pack::pack(&challenge_target_index))
-                    .target_type(ChallengeTargetType::Withdrawal.into())
-                    .block_hash(Pack::pack(&challenged_block.hash()))
-                    .build(),
-            )
-            .build();
-        let cell = build_rollup_locked_cell(
-            &rollup_type_script.hash(),
-            &challenge_script_type_hash,
-            challenge_capacity,
-            lock_args.as_bytes(),
-        );
-        let out_point = ctx.insert_cell(cell, Bytes::new());
-        CellInput::new_builder().previous_output(out_point).build()
-    };
     let global_state = chain
         .local_state()
         .last_global_state()
@@ -186,88 +161,6 @@ fn verify_withdrawal_signature(
         .withdrawals()
         .get(challenge_target_index as usize)
         .unwrap();
-    let challenge_witness = {
-        let witness = {
-            let withdrawal_proof: Bytes = {
-                let mut tree: gw_common::smt::SMT<DefaultStore<H256>> = Default::default();
-                for (index, withdrawal) in challenged_block.withdrawals().into_iter().enumerate() {
-                    tree.update(
-                        H256::from_u32(index as u32),
-                        withdrawal.witness_hash().into(),
-                    )
-                    .unwrap();
-                }
-                tree.merkle_proof(vec![H256::from_u32(challenge_target_index as u32)])
-                    .unwrap()
-                    .compile(vec![(
-                        H256::from_u32(challenge_target_index as u32),
-                        withdrawal.witness_hash().into(),
-                    )])
-                    .unwrap()
-                    .0
-                    .into()
-            };
-            let db = chain.store().begin_transaction();
-            let state_db = StateDBTransaction::from_version(
-                &db,
-                StateDBVersion::from_history_state(
-                    &db,
-                    gw_types::prelude::Unpack::unpack(&challenged_block.raw().parent_block_hash()),
-                    None,
-                )
-                .unwrap(),
-            )
-            .unwrap();
-            let mut tree = state_db.account_state_tree().unwrap();
-            tree.tracker_mut().enable();
-            let sender_id = tree
-                .get_account_id_by_script_hash(&sender_script.hash().into())
-                .unwrap()
-                .unwrap();
-            tree.get_nonce(sender_id).unwrap();
-            let account_count = tree.get_account_count().unwrap();
-            let touched_keys: Vec<H256> = tree
-                .tracker_mut()
-                .touched_keys()
-                .unwrap()
-                .borrow()
-                .clone()
-                .into_iter()
-                .collect();
-            let kv_state = touched_keys
-                .iter()
-                .map(|k| {
-                    let v = tree.get_raw(k).unwrap();
-                    (*k, v)
-                })
-                .collect::<Vec<(H256, H256)>>();
-            let kv_state_proof: Bytes = {
-                let smt = state_db.account_smt().unwrap();
-                smt.merkle_proof(touched_keys)
-                    .unwrap()
-                    .compile(kv_state.clone())
-                    .unwrap()
-                    .0
-                    .into()
-            };
-            // we do not actually execute the signature verification in this test
-            let context = VerifySignatureContext::new_builder()
-                .scripts(ScriptVec::new_builder().push(sender_script.clone()).build())
-                .account_count(Pack::pack(&account_count))
-                .kv_state(kv_state.pack())
-                .build();
-            VerifyWithdrawalWitness::new_builder()
-                .raw_l2block(challenged_block.raw())
-                .kv_state_proof(Pack::pack(&kv_state_proof))
-                .withdrawal_request(withdrawal.clone())
-                .withdrawal_proof(Pack::pack(&withdrawal_proof))
-                .context(context)
-                .build()
-        };
-        ckb_types::packed::WitnessArgs::new_builder()
-            .lock(CKBPack::pack(&Some(witness.as_bytes())))
-            .build()
-    };
     // Unlock cell's owner
     let (owner_lock_hash, owner_cell_input) = {
         let owner_cell = build_always_success_cell(42, None);
@@ -307,8 +200,6 @@ fn verify_withdrawal_signature(
     )
     .as_advanced_builder()
     .witness(CKBPack::pack(&witness.as_bytes()))
-    .input(input_challenge_cell)
-    .witness(CKBPack::pack(&challenge_witness.as_bytes()))
     .input(input_unlock_cell)
     .witness(Default::default())
     .input(owner_cell_input)
