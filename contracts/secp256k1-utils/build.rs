5e8c2e309e0f850506a4f3104148503207adffa2
jjy <jjyruby@gmail.com>
2021-06-30 23:53:26 +0800

    Fix eth-account-lock & tron-account-lock binary size

--- contracts/secp256k1-utils/build.rs
+++ contracts/secp256k1-utils/build.rs
@@ -3,12 +3,39 @@ use std::path::Path;
 
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let dir = Path::new(&dir)
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("c/build");
-    println!("cargo:rustc-link-search=native={}", dir.display());
-    println!("cargo:rustc-link-lib=static=ckb-secp256k1");
+    let root_dir = Path::new(&dir).parent().unwrap().parent().unwrap();
+    env::set_current_dir(root_dir).unwrap();
+    let mut build = cc::Build::new();
+
+    build
+        .file("c/account_lock_lib/secp256k1.c")
+        .static_flag(true)
+        .flag("-O3")
+        .flag("-fno-builtin-printf")
+        .flag("-fno-builtin-memcmp")
+        .flag("-nostdinc")
+        .flag("-nostdlib")
+        .flag("-fvisibility=hidden")
+        .flag("-Wl,-static")
+        .flag("-fdata-sections")
+        .flag("-ffunction-sections")
+        .flag("-Wl,--gc-sections")
+        // secp256k1
+        .include("c/deps/ckb-production-scripts/c")
+        .include("c/deps/ckb-production-scripts/build")
+        .include("c/deps/ckb-production-scripts/deps/secp256k1/src")
+        .include("c/deps/ckb-production-scripts/deps/secp256k1")
+        // ckb-c-stdlib
+        .include("c/deps/ckb-c-stdlib")
+        .include("c/deps/ckb-c-stdlib/libc")
+        .include("c/deps/molecule")
+        .include("c/build")
+        .flag("-Wall")
+        .flag("-Werror")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-nonnull")
+        .flag("-Wno-nonnull-compare")
+        .flag("-Wno-unused-function")
+        .define("__SHARED_LIBRARY__", None)
+        .compile("ckb-secp256k1");
 }
