eed74ffb8b960cf6b5f23b09358b2ee310149ca7
jjy <jjyruby@gmail.com>
2021-07-01 00:08:50 +0800

    Support building secp256k1-utils in non-riscv64 environments

--- contracts/secp256k1-utils/build.rs
+++ contracts/secp256k1-utils/build.rs
@@ -5,8 +5,10 @@ fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let root_dir = Path::new(&dir).parent().unwrap().parent().unwrap();
     env::set_current_dir(root_dir).unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let mut build = cc::Build::new();
 
+    // building
     build
         .file("c/account_lock_lib/secp256k1.c")
         .static_flag(true)
@@ -36,6 +38,13 @@ fn main() {
         .flag("-Wno-nonnull")
         .flag("-Wno-nonnull-compare")
         .flag("-Wno-unused-function")
-        .define("__SHARED_LIBRARY__", None)
-        .compile("ckb-secp256k1");
+        .define("__SHARED_LIBRARY__", None);
+
+    if target_arch == "riscv64" {
+        build.flag("-nostartfiles").compile("ckb-secp256k1");
+    } else {
+        // skip build for non riscv64
+        build.define("CKB_STDLIB_NO_SYSCALL_IMPL", None);
+        build.compile("ckb-secp256k1");
+    }
 }
