1a9d92baa46b8ee72d5114ce3bc6fbc690ed9ccb
Flouse <flouse@gmail.com>
2021-10-08 17:02:32 +0800

    fix(limitations): tune GW_MAX_DATA_SIZE to 25KB
    https://github.com/nervosnetwork/godwoken-polyjuice/issues/92

--- c/gw_def.h
+++ c/gw_def.h
@@ -18,8 +18,8 @@ typedef unsigned __int128 uint128_t;
 #define GW_SHORT_ACCOUNT_SCRIPT_HASH_TO_SCRIPT_HASH 5
 
 /* Limitations */
-/* 24KB (ethereum max contract code size) */
-#define GW_MAX_DATA_SIZE (24 * 1024)
+/* 25KB (ethereum max contract code size) */
+#define GW_MAX_DATA_SIZE (25 * 1024)
 /* 128KB */
 #define GW_MAX_L2TX_ARGS_SIZE (128 * 1024)
 /* 128KB + 4KB */
