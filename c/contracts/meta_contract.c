5d282a632a308f5190171afa49a89a52266c2f5b
zero.qn <zero.qn@outlook.com>
2022-05-28 12:14:39 +0800

    feat(meta_contract): batch create eth accounts

--- c/contracts/meta_contract.c
+++ c/contracts/meta_contract.c
@@ -7,11 +7,13 @@
  */
 
 #include "ckb_syscalls.h"
+#include "gw_eth_addr_reg.h"
 #include "gw_syscalls.h"
 #include "sudt_utils.h"
 
 /* MSG_TYPE */
 #define MSG_CREATE_ACCOUNT 0
+#define MSG_BATCH_CREATE_ACCOUNTS 1
 
 int handle_fee(gw_context_t *ctx, uint32_t registry_id, uint256_t amount) {
   if (ctx == NULL) {
@@ -95,6 +97,64 @@ int main() {
                                           sizeof(uint32_t));
     if (ret != 0) {
       ckb_debug("failed to set return data");
+      return ret;
+    }
+  } else if (msg.item_id == MSG_BATCH_CREATE_ACCOUNTS) {
+    /* charge fee */
+    mol_seg_t fee_seg = MolReader_BatchCreateEthAccounts_get_fee(&msg.seg);
+    mol_seg_t amount_seg = MolReader_Fee_get_amount(&fee_seg);
+    mol_seg_t reg_id_seg = MolReader_Fee_get_registry_id(&fee_seg);
+
+    uint256_t fee_amount = {0};
+    _gw_fast_memcpy((uint8_t *)(&fee_amount), (uint8_t *)amount_seg.ptr,
+                    sizeof(uint128_t));
+
+    uint32_t reg_id = *(uint32_t *)reg_id_seg.ptr;
+    ret = handle_fee(&ctx, reg_id, fee_amount);
+    if (ret != 0) {
+      ckb_debug("failed to handle fee");
+      return ret;
+    }
+
+    /* create accounts */
+    mol_seg_t scripts_seg =
+        MolReader_BatchCreateEthAccounts_get_scripts(&msg.seg);
+    uint32_t scripts_size = MolReader_ScriptVec_length(&scripts_seg);
+
+    uint32_t account_id = 0;
+    uint8_t account_script_hash[32] = {0};
+    for (uint32_t i = 0; i < scripts_size; i++) {
+      mol_seg_res_t script_res = MolReader_ScriptVec_get(&scripts_seg, i);
+      if (script_res.errno != MOL_OK) {
+        ckb_debug("invalid account script");
+        return GW_FATAL_INVALID_DATA;
+      }
+
+      ret = ctx.sys_create(&ctx, script_res.seg.ptr, script_res.seg.size,
+                           &account_id);
+      if (ret != 0) {
+        ckb_debug("failed to create eth account");
+        return ret;
+      }
+
+      ret = ctx.sys_get_script_hash_by_account_id(&ctx, account_id,
+                                                  account_script_hash);
+      if (ret != 0) {
+        ckb_debug("failed to get created eth account script hash");
+        return ret;
+      }
+
+      ret = gw_register_eth_address(&ctx, account_script_hash);
+      if (ret != 0) {
+        ckb_debug("failed to register eth address");
+        return ret;
+      }
+    }
+
+    ret = ctx.sys_set_program_return_data(&ctx, (uint8_t *)&account_id,
+                                          sizeof(uint32_t));
+    if (ret != 0) {
+      ckb_debug("failed to set return data to last created eth account id");
       return ret;
     }
   } else {
